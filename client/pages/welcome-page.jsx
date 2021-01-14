e87402e6e0c3728b00d363b2870bf3a3a73762b2
Viviane Vieira <viviane.kodama@gmail.com>
2021-01-14 01:13:05 +0000

    Added a description of the application to the home page.

--- client/pages/welcome-page.jsx
+++ client/pages/welcome-page.jsx
@@ -11,6 +11,19 @@ function WelcomePage() {
       <div className="">
         <p className="welcome-title fs-6 text-center">Custom CRM for dog trainers</p>
       </div>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-6 col-lg-4 col-sm mb-2">
+          <i className="fas fa-graduation-cap mb-2"></i>
+          <p>DogNet is a Customer Relationship Management tool for dog trainers
+            built as a final project while attending a full stack web development
+            bootcamp at LearningFuze.</p>
+        </div>
+        <div className="col-md-6 col-lg-4 col-sm">
+          <i className="fas fa-users mb-2"></i>
+          <p>The goal was to create a solution to allow users to
+             conveniently enter and access key information about their clients.</p>
+        </div>
+      </div>
     </div>
   );
 }
